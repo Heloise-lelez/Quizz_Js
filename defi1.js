9ab2102e13d5819c66565b709e9453fc0779ba68
Mohamed <mohameddrame907@gmail.com>
2021-10-21 09:01:43 +0200

    defi1 changement de question

--- defi1.js
+++ defi1.js
@@ -7,8 +7,7 @@ let regles = document.getElementById("regles");
 
 let divquestion = document.createElement('div');
 
-let divreponse = document.createElement('div');
-
+let allbutton = document.getElementsByTagName('button');
 
     /* Déclaration des variables du timer */
 let timer = document.getElementById("tmp");
@@ -16,14 +15,12 @@ let temps1 = 120;
 
 
 
-let quizz = allQuestions[77].quizz;
-
-
 
 let paraquestion = document.createElement('p');
 
 
 let listerep = document.createElement('div');
+
 
 
 
@@ -60,11 +57,28 @@ function perdTemps(){
 
 function defi1(){
 
+
     
     regles.remove();    // supprime tout les éléments de la div regles
+    let rand = Math.floor(Math.random()*allQuestions.length);
+
+
+    for(let i =0;i<allbutton.length;i++){           //affichage de la couleur au survol de chaque réponse
+        allbutton[i].addEventListener('mouseover',function(){
+            console.log(allbutton[i]);
+            allbutton[i].style.color =red;
+
+        })
+    }
+
+    let divreponse = document.createElement('div');
+
+
+    let quizz = allQuestions[rand].quizz;
 
 
     timer.innerHTML =(temps1);
+    
     setInterval(DiminuerTemps1, 1000); // diminuer le temps à chaque seconde
 
 
@@ -75,54 +89,86 @@ function defi1(){
     paraquestion.innerHTML = quizz;
 
 
-    for(let key in allQuestions[55]){
-        let goodrep = allQuestions[77]["goodrep"];
+    for(let key in allQuestions[rand]){
+
+        let goodrep = allQuestions[rand]["goodrep"];
         //console.log(goodrep)
         
         // if(key == "rep1" || key == "rep2" || key == "rep3" || key == "rep4"){
         //     let w = window.localStorage.setItem(key,allQuestions[44][key])
         // }
         
-        let repdefi1 = localStorage.getItem("i");
-        console.log(repdefi1)
+        //let repdefi1 = localStorage.getItem("i");
+        //console.log(repdefi1)
 
 
         if(key == "goodrep"){
-            //console.log(allQuestions[77][key])
+            //console.log(allQuestions[rand][key])
         }
         else if(key == "rep1"){
             let buttonrep1 = document.createElement('button');
             divreponse.appendChild(buttonrep1);
 
-            buttonrep1.innerHTML = allQuestions[77][key];
+            buttonrep1.innerHTML = allQuestions[rand][key];
             if(key.charAt(3) == goodrep){
                 //console.log("Bonne réponse 1")
                 buttonrep1.addEventListener('click',ajoutTemps);
+                buttonrep1.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();
+                 })
             }else{
                 buttonrep1.addEventListener('click',perdTemps);
+                buttonrep1.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();
+                 })
             }
 
         }else if(key == "rep2"){
             let buttonrep2 = document.createElement('button');
             divreponse.appendChild(buttonrep2);
 
-            buttonrep2.innerHTML = allQuestions[77][key];
+            buttonrep2.innerHTML = allQuestions[rand][key];
             if(key.charAt(3) == goodrep){
                // console.log("Bonne réponse 2")
                buttonrep2.addEventListener('click',ajoutTemps);
+               buttonrep2.addEventListener('click',function(){
+                divreponse.remove();
+                divquestion.remove();
+                defi1();
+            })
             }else{
                 buttonrep2.addEventListener('click',perdTemps);
+                buttonrep2.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();
+                })
             }
         }else if(key == "rep3"){
             let buttonrep3 = document.createElement('button');
             divreponse.appendChild(buttonrep3);
 
-            buttonrep3.innerHTML = allQuestions[77][key];
+            buttonrep3.innerHTML = allQuestions[rand][key];
             if(key.charAt(3) == goodrep){
                 //console.log("Bonne réponse 3")
                 buttonrep3.addEventListener('click',ajoutTemps);
+                buttonrep3.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();               
+                })
+                
             }else{
                 buttonrep3.addEventListener('click',perdTemps);
+                buttonrep3.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();               
+                })
 
             }
         
@@ -130,12 +176,22 @@ function defi1(){
             let buttonrep4 = document.createElement('button');
             divreponse.appendChild(buttonrep4);
 
-            buttonrep4.innerHTML = allQuestions[77][key];
+            buttonrep4.innerHTML = allQuestions[rand][key];
             if(key.charAt(3) == goodrep){
                 //console.log("Bonne réponse 4")
                 buttonrep4.addEventListener('click',ajoutTemps);
+                buttonrep4.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();
+                })
             }else{
                 buttonrep4.addEventListener('click',perdTemps);
+                buttonrep4.addEventListener('click',function(){
+                    divreponse.remove();
+                    divquestion.remove();
+                    defi1();
+                })
 
             }
         
